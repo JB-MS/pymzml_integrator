22491e29ea865ae33d22e66d212ac22c469348c7
Johannes Leufken <johannes.leufken@mpi-muenster.mpg.de>
2017-05-10 14:25:13 +0200

    Adjusted structure to prepare import

---
+++ simple_mzid_parser.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.4
+# encoding: utf-8
+
+import os
+import sys
+from pymzid import Mzid
+
+
+def main():
+    '''
+    Simple parser for Mzid files based on pymzl_integrator module
+
+    © Johannes Leufken 2017
+
+    '''
+    reader = Mzid(sys.argv[1])
+    data_frame = reader.peptide_df
+    for seq in data_frame['seq']:
+        print(seq)
+    return
+
+if __name__ == '__main__':
+    main()
